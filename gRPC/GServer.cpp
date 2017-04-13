5dd2890dcc6bdcc2540d1ddfb7022e5df95221fe
cramja <marc.spehlmann@gmail.com>
2017-04-12 22:56:21 -0500

    Cleaned up TCP code

---
+++ gRPC/GServer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Marc Spehlmann on 4/5/17.
+//
+
+#include "GServer.hpp"
+
+int main(int args, char** argv) {
+  return 0;
+}
---
+++ gRPC/GServer.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Marc Spehlmann on 4/5/17.
+//
+
+#ifndef TCPSERVER_GSERVER_HPP
+#define TCPSERVER_GSERVER_HPP
+
+
+class GServer {
+
+};
+
+
+#endif //TCPSERVER_GSERVER_HPP
